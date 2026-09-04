73bc73ae8fa22f0056dc483f7b3cad534fca2941
yutaka <yutaka.juli@gmail.com>
2026-09-04 23:06:28 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,5 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
 export default {
   output: isGitHubPages ? 'export' : undefined,
-  basePath: isGitHubPages ? '/ring-reversal-tactics' : undefined,
+  assetPrefix: isGitHubPages ? './' : undefined,
 };
