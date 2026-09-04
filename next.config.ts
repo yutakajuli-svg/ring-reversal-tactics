bb1776a51ca365c579520d4bfeba7508068814e2
yutaka <yutaka.juli@gmail.com>
2026-09-04 23:06:47 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,5 +2,4 @@ const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
 export default {
   output: isGitHubPages ? 'export' : undefined,
-  assetPrefix: isGitHubPages ? './' : undefined,
 };
