588e234622e7f0a6f232198b178b32865fc4ab4b
yutaka <yutaka.juli@gmail.com>
2026-09-08 23:44:31 +0900

    Swap wrestler rotation icons

--- app/ring-lab/page.tsx
+++ app/ring-lab/page.tsx
@@ -1973,25 +1973,25 @@ export default function RingLabPage() {
               style={{ ...boardPosition(wrestlers.red.location, boardRotation), zIndex: 145 }}
             >
               <button
-                aria-label="左へ90度回転"
+                aria-label="右へ90度回転"
                 disabled={actionLocksBoard}
                 onClick={() => setActiveFacing(turnFacing(wrestlers.red.facing, -1))}
                 type="button"
               >
                 <svg aria-hidden="true" viewBox="0 0 24 24">
-                  <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
-                  <path d="M3 3v5h5" />
+                  <path d="M21 12a9 9 0 1 1-9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+                  <path d="M21 3v5h-5" />
                 </svg>
               </button>
               <button
-                aria-label="右へ90度回転"
+                aria-label="左へ90度回転"
                 disabled={actionLocksBoard}
                 onClick={() => setActiveFacing(turnFacing(wrestlers.red.facing, 1))}
                 type="button"
               >
                 <svg aria-hidden="true" viewBox="0 0 24 24">
-                  <path d="M21 12a9 9 0 1 1-9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
-                  <path d="M21 3v5h-5" />
+                  <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+                  <path d="M3 3v5h5" />
                 </svg>
               </button>
             </div>
