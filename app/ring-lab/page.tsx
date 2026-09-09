29a9480ffdf9ddc5c87a8a7d4b6305de9cf75fbe
yutaka <yutaka.juli@gmail.com>
2026-09-09 20:48:45 +0900

    Center down-state wrestler sprites

--- app/ring-lab/page.tsx
+++ app/ring-lab/page.tsx
@@ -697,7 +697,7 @@ function WrestlerCube({
               : `url(${ASSET_BASE}/assets/wrestler-${characterSprite}-rounded-sprites.png)`,
             backgroundPosition: down ? 'center' : spritePosition[facing],
             backgroundSize: down ? 'contain' : '200% 200%',
-            transform: down ? 'translate(0, 4px) scale(1.14)' : spriteTransform[facing],
+            transform: down ? 'translate(-10px, -3px) scale(.92)' : spriteTransform[facing],
           }}
         />
       )}
